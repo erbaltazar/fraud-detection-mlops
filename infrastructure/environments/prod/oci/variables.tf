505733dea725df8231b1e618776980620cb39724
Eufronio Baltazar <eufroniobaltazar@gmail.com>
2026-08-11 17:51:15 +0900

    fix: declare infisical client variables in root module

--- infrastructure/environments/prod/oci/variables.tf
+++ infrastructure/environments/prod/oci/variables.tf
@@ -6,3 +6,13 @@ variable "region" { type = string }
 variable "compartment_ocid" { type = string }
 variable "ssh_public_key" { type = string }
 variable "local_ip" { type = string }
+variable "infisical_client_id" {
+  type        = string
+  description = "Infisical Universal Auth Client ID"
+  sensitive   = true
+}
+variable "infisical_client_secret" {
+  type        = string
+  description = "Infisical Universal Auth Client Secret"
+  sensitive   = true
+}
