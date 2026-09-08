9d771d4771b8c0e7a76bf9d24cfcdfdcfa3593ae
Eufronio Baltazar <eufroniobaltazar@gmail.com>
2026-09-08 20:58:50 +0900

    hack: bypass tls verification for external k3s connection

--- infrastructure/environments/prod/oci/main.tf
+++ infrastructure/environments/prod/oci/main.tf
@@ -60,17 +60,17 @@ locals {
 
 provider "kubernetes" {
   host                   = local.kubeconfig.clusters[0].cluster.server
-  cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster["certificate-authority-data"])
   client_certificate     = base64decode(local.kubeconfig.users[0].user["client-certificate-data"])
   client_key             = base64decode(local.kubeconfig.users[0].user["client-key-data"])
+  insecure               = true
 }
 
 provider "helm" {
   kubernetes {
     host                   = local.kubeconfig.clusters[0].cluster.server
-    cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster["certificate-authority-data"])
     client_certificate     = base64decode(local.kubeconfig.users[0].user["client-certificate-data"])
     client_key             = base64decode(local.kubeconfig.users[0].user["client-key-data"])
+    insecure               = true
   }
 }
 
